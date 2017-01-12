06c85d5840ea1d03930211fcd73f185983cd25b6
Brendan Murtagh <brendan.murtagh@ellucian.com>
2017-01-12 13:22:29 -0500

    added tomcat init.d creation and service definition steps

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -29,6 +29,42 @@ node default {
     require => File['/opt/tomcat/conf/context.xml'],
   }
 
+  # content for tomcat init.d script
+  $tomcat_initd_content = "#!/bin/bash
+# description: Tomcat Start Stop Restart
+# processname: tomcat
+# chkconfig: 234 20 80
+CATALINA_HOME=/opt/tomcat
+
+case \$1 in
+start)
+sh \$CATALINA_HOME/bin/startup.sh
+;;
+stop)
+sh \$CATALINA_HOME/bin/shutdown.sh
+;;
+restart)
+sh \$CATALINA_HOME/bin/shutdown.sh
+sh \$CATALINA_HOME/bin/startup.sh
+;;
+esac
+exit 0
+  "
+
+  # create tomcat init.d script from $tomcat_initd_content
+  file { '/etc/init.d/tomcat':
+    ensure => present,
+    content => $tomcat_initd_content,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '755',
+  } ->
+
+  service { 'tomcat':
+    ensure => running,
+    enable => true,
+  }
+
   # create nabudev directories & set permissions/ownership
   $nabudev_directories = ['/datam', '/datam/nabudata', '/datam/nabudata/ngif', '/datam/nabudata/npdf', '/datam/nabudata/nxml']
 
